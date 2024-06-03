0e43be44031c4ae5c2ca7b8e5a1deae64ad30282
Mohamed El attar <mohamadelattar99@gmail.com>
2024-06-03 21:35:23 +0100

    "Initial commit"

---
+++ src/main/java/com/rs/rsspring/RsSpringApplication.java
@@ -0,0 +1,13 @@
+package com.rs.rsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rs/rsspring/RsSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rs.rsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
